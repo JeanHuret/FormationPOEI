48550f32175524ed54757a01cfb7141087c40f7a
Arn0960 <105207681+Arn0960@users.noreply.github.com>
2022-05-31 12:59:00 +0000

    Add files via upload

---
+++ Projet_final/data_base_quizz.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS data_base_quizz;
+USE data_base_quizz;
+CREATE TABLE IF NOT EXISTS quizz(
+    Question VARCHAR(200) NOT NULL
+    ,Reponsea VARCHAR(200) NOT NULL
+    ,Reponseb VARCHAR(200) NOT NULL
+    ,Reponsec VARCHAR(200) NOT NULL
+);
+INSERT INTO quizz(Question,Reponsea,Reponseb,Reponsec) VALUES ('Quelle est la capitale de France','Lyon','Paris','Lisbonne');
+INSERT INTO quizz(Question,Reponsea,Reponseb,Reponsec) VALUES ('Comment dit-on salut en Anglais','Enrevoir','Hello','Jacket');
+INSERT INTO quizz(Question,Reponsea,Reponseb,Reponsec) VALUES ('Combien d étoile possède le drapeau européen','2','12','30');
+INSERT INTO quizz(Question,Reponsea,Reponseb,Reponsec) VALUES ('Combien d espece d insecte a t on découvert à ce jour','500 000','1 million','Plus de 5 millions');
+INSERT INTO quizz(Question,Reponsea,Reponseb,Reponsec) VALUES ('En quelle année est sortie la 1er console salon de jeux vidéo','1982','1972','1990');
+INSERT INTO quizz(Question,Reponsea,Reponseb,Reponsec) VALUES ('En quelle année est sortie la console nes','1975','1983','1991');
+INSERT INTO quizz(Question,Reponsea,Reponseb,Reponsec) VALUES ('En quelle année le TITANIC a t il coulé','2010','1912','1850');
+INSERT INTO quizz(Question,Reponsea,Reponseb,Reponsec) VALUES ('Dans quel pays se trouve les jardins suspendus de Babylone (1 des 7 merveilles)','Iran','Irak','Bilbéis');
+INSERT INTO quizz(Question,Reponsea,Reponseb,Reponsec) VALUES ('Combien de couleur y a til dans l arc en ciel','10','7','5');
+INSERT INTO quizz(Question,Reponsea,Reponseb,Reponsec) VALUES ('Dans qulle ville de france se situe la tour effeil','Lyon','Paris','Marseille');
+INSERT INTO quizz(Question,Reponsea,Reponseb,Reponsec) VALUES ('En quelle année la france remporte t ell la premiere coupe du monde','2000','1998','1990');
+INSERT INTO quizz(Question,Reponsea,Reponseb,Reponsec) VALUES ('eded','sdd','ffdffg')
